fed0577124d7c9fcb3220348bf26c81bfe389518
Gareth Fleming <gareth@joocy.com>
2024-11-11 13:44:20 +0000

    report unknown command

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -6,12 +6,15 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Fprint
-
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		os.Exit(1)
+	}
+	input = input[:len(input)-1] // Remove trailing newline
+	fmt.Printf("%s: command not found\n", input)
 }
