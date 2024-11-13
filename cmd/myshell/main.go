b3638bf92ce6a2b3ccf90e0381081493f7acc6ef
Gareth Fleming <gareth@joocy.com>
2024-11-13 13:40:37 +0000

    repl

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,14 +7,15 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+		input = input[:len(input)-1] // Remove trailing newline
+		fmt.Printf("%s: command not found\n", input)
 	}
-	input = input[:len(input)-1] // Remove trailing newline
-	fmt.Printf("%s: command not found\n", input)
 }
