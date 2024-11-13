fcbb589f7bce0013fc26a831161622a06b7bf891
Gareth Fleming <gareth@joocy.com>
2024-11-13 13:47:32 +0000

    implement exit builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -16,6 +18,29 @@ func main() {
 			os.Exit(1)
 		}
 		input = input[:len(input)-1] // Remove trailing newline
+
+		command, args := parseInput(input)
+		
+		if command == "exit" {
+			if len(args) != 1 {
+				fmt.Fprintln(os.Stderr, "usage: exit <exit code>")
+				continue
+			} else {
+				exitCode, err := strconv.Atoi(args[0])
+				if err != nil {
+					fmt.Fprintln(os.Stderr, "Invalid exit code:", args[0])
+					os.Exit(1)
+				}
+
+				os.Exit(exitCode)
+			}
+		}
+
 		fmt.Printf("%s: command not found\n", input)
 	}
+}
+
+func parseInput(input string) (command string, args []string) {
+	parsed := strings.Split(input, " ")
+	return parsed[0], parsed[1:]
 }
